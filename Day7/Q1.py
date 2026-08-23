1ddaa6399dd4c1215c7fe37098c2cafc043c30ce
Debasish-Nayak-git <debasishn185@gmail.com>
2026-08-23 20:28:55 +0530

    some changes

--- Day7/Q1.py
+++ Day7/Q1.py
@@ -1,8 +1,10 @@
 #generator function
 sub=["math","chem","phy","bee","c"]
-mark=[97,94,95,95,98]
+mark,sum=[97,94,95,95,98],0
 def gen(n):
     for i in range (n):
         yield i
 for j in gen(len(mark)):
     print(sub[j],":",mark[j])
+    sum +=mark[j]
+print("total marks =",sum)
